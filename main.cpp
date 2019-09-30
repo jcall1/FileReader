c835956889fe015395213b5d9311a8ec3f1932e1
jcall1 <jcall1@mail.csuchico.edu>
2019-09-30 16:11:02 -0700

    Read a file entered by user

--- main.cpp
+++ main.cpp
@@ -14,16 +14,22 @@ int main()
   getline(cin, fileName);
   
   //STEP 1: open the fileStream for input, using the fileName specified
+  fileStream.open (fileName, fstream::in);
   
-  if( /*STEP 2: check to see if the fileStream successfully opened*/ )
+  if(fileStream.is_open() /*STEP 2: check to see if the fileStream successfully opened*/ )
   {
     cout<<fileName<<" opened.\nFILE CONTENTS:\n";
     
     //STEP 3: repeat the following until the end-of-file (eof) has been reached...
+    while(!fileStream.eof()){ 
     // 3A: read a line from fileStream into the variable line
+    getline(fileStream, line);
     // 3B: display the line, followed by an endline
+    cout<<line<<endl;
+    }
   
     //STEP 4: close the fileStream
+    fileStream.close();
   
   }
   else
